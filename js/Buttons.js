eb11dfb102d865402e50d2a1d7637e01e0423f05
operolim <operolim2003@yahoo.com>
2015-10-25 06:24:37 -0500

    adding video to exten.

--- js/Buttons.js
+++ js/Buttons.js
@@ -5,21 +5,33 @@ function viewTabs()
     chrome.tabs.query(queryInfo, function(tabs)
     {
         var currentTabUrl = "";
+        var urlLength
+        var id = "";
         document.getElementById("openLinks").innerHTML = "";
-        
+
         for (i = 0; i < tabs.length; i++)
         {
-            document.getElementById("openLinks").innerHTML += "<li>" + tabs[i].title + "</li>";
+            id = tabs[i].url.slice(-11);
+            urlLength = tabs[i].url.length;
+            currentTabUrl = tabs[i].url.slice(0,urlLength-11) + id;
+            if(tabs[i].url.indexOf("youtube") > -1) {
+                //document.getElementById("openLinks").innerHTML += "<li> <a href=" + tabs[i].url + ">" + tabs[i].title + "</a> </li>";
+                $.getJSON('https://noembed.com/embed',
+                    {format: 'json', url: currentTabUrl}, function (data) {
+                        $("#openLinks").append("<li> <a href=" + data.url + ">" + data.title + "</a> </li>").click(console.log(data.url))
+                        //$("#videoFrame")
+                        //$("#je").attr('href', data.url).attr('target','_self').text(data.title)
+                    });
+            }
         }
     });
 }
 
-function popupStatus(statusText)
-{
-    document.getElementById('StatusLabel').textContent = statusText;
-}
+$(document).click(function(){
+    var id = document.activeElement.attributes.href.nodeValue.slice(-11)
+    var file = "https://www.youtube-nocookie.com/embed/" + id +"?autoplay=1";
+    document.getElementById("videoFrame").setAttribute("src",file);
+    console.log(file)
+})
 
 viewTabs();
-
-// FUNCTION CHAINGING is messed up
-document.getElementById("SaveLinks").addEventListener("click", function() { SaveLinks() });
